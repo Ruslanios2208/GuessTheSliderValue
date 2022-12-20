be7e40f0e93eeb9faec5d3021174d187cab9e8f1
Ruslanios22 <ilgamovich@gmail.com>
2022-12-20 21:55:28 +0300

    Initial Commit

---
+++ GuessTheSliderValue/GuessTheSliderValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheSliderValueApp.swift
+//  GuessTheSliderValue
+//
+//  Created by Руслан Шигапов on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheSliderValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
